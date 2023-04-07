95a6bc51cc5deca27c9fbbb71141a03a0c58e188
poya <poya1381@gmail.com>
2023-04-07 13:10:05 +0430

    add-strings

--- lib/modules/extensions/double.dart
+++ lib/modules/extensions/double.dart
@@ -2,20 +2,35 @@ import '../../root/root.dart';
 
 // todo => extension / double
 
-extension TheDouble on Widget{
-   toPadding({double all = 0}) => Padding(padding: EdgeInsets.all(0),child: this,);
-   toSymmetric({
-     double h = 0,
-     double v = 0,
-   }) => Padding(padding: EdgeInsets.symmetric(vertical: v,horizontal:h ),child: this,);
-   toOnly({
-     double left = 0,
-     double top = 0,
-     double right = 0,
-     double bottom = 0
-   }) => Padding(padding: EdgeInsets.fromLTRB(left,top,right,bottom),child: this,);
+extension TheDouble on Widget {
+  toPadding({double all = 0}) => Padding(
+        padding: const EdgeInsets.all(0),
+        child: this,
+      );
+  toSymmetric({
+    double h = 0,
+    double v = 0,
+  }) =>
+      Padding(
+        padding: EdgeInsets.symmetric(vertical: v, horizontal: h),
+        child: this,
+      );
+  toOnly(
+          {double left = 0,
+          double top = 0,
+          double right = 0,
+          double bottom = 0}) =>
+      Padding(
+        padding: EdgeInsets.fromLTRB(left, top, right, bottom),
+        child: this,
+      );
 }
-extension TheDouble2 on double{
-  toWidth({required BuildContext context}) => SizedBox(width: MediaQuery.of(context).size.width,);
-  toHeight({required BuildContext context}) => SizedBox(height: MediaQuery.of(context).size.height,);
+
+extension TheDouble2 on double {
+  toWidth({required BuildContext context}) => SizedBox(
+        width: MediaQuery.of(context).size.width,
+      );
+  toHeight({required BuildContext context}) => SizedBox(
+        height: MediaQuery.of(context).size.height,
+      );
 }
--- lib/modules/extensions/string.dart
+++ lib/modules/extensions/string.dart
@@ -2,3 +2,19 @@ import '../../root/root.dart';
 
 // todo => extension / String
 
+extension TheString on String {
+  get bodySmall => Text(
+        bodySmall,
+        style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+      );
+
+  get bodyMedium => Text(
+        bodyMedium,
+        style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+      );
+
+  get bodyLarge => Text(
+        bodyLarge,
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      );
+}
