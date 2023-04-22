3940a481d48a1d6ccc62640a040f38cc1a2f6c2a
DanialMirzaei23 <danialsly23@gmail.com>
2023-04-22 14:51:41 +0330

    Features Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,39 @@
-import 'root/root.dart';
+export 'root/root.dart';
 
-void main() => runApp(
-      const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Tester(
-          widget: Text("data"),
-        ),
-      ),
-    );
+// void main() => runApp(
+//       const MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: Tester(
+//           widget: Text("data"),
+//         ),
+//       ),
+//     );
 
-class Tester extends StatelessWidget {
-  const Tester({Key? key, required this.widget}) : super(key: key);
-  final Widget widget;
-
-  @override
-  Widget build(BuildContext context) {
-    TextEditingController controller = TextEditingController();
-
-    return Scaffold(
-      body: Center(
-        child: TextFormField().toFormFieldFullUp(
-          controller: controller,
-          onChanged: (value) {
-            print(value);
-          },
-          kyboardType: TextInputType.text,
-          onTap: () {
-            print('hiiiii');
-          },
-          decoration: const InputDecoration(
-            filled: true,
-            fillColor: Colors.teal,
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class Tester extends StatelessWidget {
+//   const Tester({Key? key, required this.widget}) : super(key: key);
+//   final Widget widget;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     TextEditingController controller = TextEditingController();
+//
+//     return Scaffold(
+//       body: Center(
+//         child: TextFormField().toFormFieldFullUp(
+//           controller: controller,
+//           onChanged: (value) {
+//             print(value);
+//           },
+//           kyboardType: TextInputType.text,
+//           onTap: () {
+//             print('hiiiii');
+//           },
+//           decoration: const InputDecoration(
+//             filled: true,
+//             fillColor: Colors.teal,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
