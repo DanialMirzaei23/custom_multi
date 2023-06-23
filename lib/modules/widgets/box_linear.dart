92161ede4697880566cc839f0c7f77387fbf5bc0
DanialMirzaei23 <danialsly23@gmail.com>
2023-06-23 15:24:27 +0330

    Update BoxLinear

--- lib/modules/widgets/box_linear.dart
+++ lib/modules/widgets/box_linear.dart
@@ -3,10 +3,40 @@ import '../../root/root.dart';
 // todo => Widget / box Linear
 
 class BoxLinear extends StatelessWidget {
-  const BoxLinear({Key? key}) : super(key: key);
+  const BoxLinear({
+    Key? key,
+     required this.gradient,
+     this.borderRadius=8.0,
+     this.sizeHeight = .2 ,
+     this.sizeWidth = .4 ,
+     this.fontSize = 14.0 ,
+     this.text = 'please name' ,
+     this.colorText =Colors.black,
+     this.colorBorder =Colors.black,
+     this.fontWeight =FontWeight.normal,
+     this.isBorder = false,
+  }) : super(key: key);
+  final double sizeWidth, fontSize ;
+  final double sizeHeight , borderRadius;
+  final String text ;
+  final Color colorText,colorBorder ;
+  final FontWeight fontWeight;
+  final Gradient gradient;
+  final bool isBorder ;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: text.toText(
+          color: colorText, fontSize: fontSize, fontWeight: fontWeight),
+      alignment: Alignment.center,
+      width: MediaQuery.of(context).size.width * sizeWidth,
+      height: MediaQuery.of(context).size.height * sizeHeight,
+      decoration: BoxDecoration(
+        gradient: gradient,
+        border: isBorder ? Border.all(color: colorBorder) : const Border.fromBorderSide(BorderSide.none) ,
+        borderRadius: BorderRadius.circular(borderRadius),
+      ),
+    );
   }
 }
